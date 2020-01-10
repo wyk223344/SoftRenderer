1fdc6c053ac62d28f31e8b6cff4dcd3a2d841a5a
wangyankun <1594707439@qq.com>
2020-01-10 23:24:57 +0800

    删除文件

--- SoftRenderer/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "window/Window.h"
-
-
-int main(int argc, char** argv) {
-    return 0;
-}
--- SoftRenderer/window/Window.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-class Window {
-public:
-    static Window Create(int width, int height);
-
-};
