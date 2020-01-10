e0509ba1a425d7e92a70bc9fa5e8cb67c3302221
wangyankun <1594707439@qq.com>
2020-01-10 19:09:01 +0800

    start

---
+++ SoftRenderer/main.cpp
@@ -0,0 +1,6 @@
+#include "window/Window.h"
+
+
+int main(int argc, char** argv) {
+    return 0;
+}
---
+++ SoftRenderer/window/Window.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+class Window {
+public:
+    static Window Create(int width, int height);
+
+};
